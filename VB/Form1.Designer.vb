e9aa53a097a9a825920e0993d9b327895f952433
DevExpressExampleBot <fakeEmail>
2023-12-08 11:48:33 +0400

    autogenerate VB

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,101 +1,110 @@
-<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
-    Inherits System.Windows.Forms.Form
+Namespace ConditionallyHideSeriesLabels
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
+    Partial Class Form1
+
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
-        Finally
+
             MyBase.Dispose(disposing)
-        End Try
-    End Sub
+        End Sub
 
-    'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim xyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim lineSeriesView1 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
+            Dim dataFilter1 As DevExpress.XtraCharts.DataFilter = New DevExpress.XtraCharts.DataFilter("Year", "System.Int32", DevExpress.XtraCharts.DataFilterCondition.Equal, 2003)
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            Me.gspDataSet = New ConditionallyHideSeriesLabels.gspDataSet()
+            Me.gspDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.gSPTableAdapter = New ConditionallyHideSeriesLabels.gspDataSetTableAdapters.GSPTableAdapter()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((lineSeriesView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gspDataSet), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gspDataSetBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.DataAdapter = Me.gSPTableAdapter
+            Me.chartControl1.DataSource = Me.gspDataSetBindingSource
+            xyDiagram1.AxisX.Label.Staggered = True
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisX.Range.SideMarginsEnabled = True
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisY.Range.SideMarginsEnabled = True
+            Me.chartControl1.Diagram = xyDiagram1
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Legend.Visible = False
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            series1.Name = "Series 1"
+            series1.View = lineSeriesView1
+            series1.ArgumentDataMember = "Region"
+            series1.ValueDataMembersSerializable = "GSP"
+            series1.DataFilters.ClearAndAddRange(New DevExpress.XtraCharts.DataFilter() {dataFilter1})
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {series1}
+            Me.chartControl1.Size = New System.Drawing.Size(474, 325)
+            Me.chartControl1.TabIndex = 0
+            AddHandler Me.chartControl1.CustomDrawSeriesPoint, New DevExpress.XtraCharts.CustomDrawSeriesPointEventHandler(AddressOf Me.chartControl1_CustomDrawSeriesPoint)
+            ' 
+            ' gspDataSet
+            ' 
+            Me.gspDataSet.DataSetName = "gspDataSet"
+            Me.gspDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' gspDataSetBindingSource
+            ' 
+            Me.gspDataSetBindingSource.DataMember = "GSP"
+            Me.gspDataSetBindingSource.DataSource = Me.gspDataSet
+            ' 
+            ' gSPTableAdapter
+            ' 
+            Me.gSPTableAdapter.ClearBeforeFill = True
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(474, 325)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((lineSeriesView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gspDataSet), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gspDataSetBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container
-        Dim XyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram
-        Dim Series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series
-        Dim LineSeriesView1 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView
-        Dim DataFilter1 As DevExpress.XtraCharts.DataFilter = New DevExpress.XtraCharts.DataFilter("Year", "System.Int32", DevExpress.XtraCharts.DataFilterCondition.Equal, 2003)
-        Me.ChartControl1 = New DevExpress.XtraCharts.ChartControl
-        Me.GspDataSet = New ConditionallyHideSeriesLabels.gspDataSet
-        Me.GspDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.GSPTableAdapter = New ConditionallyHideSeriesLabels.gspDataSetTableAdapters.GSPTableAdapter
-        CType(Me.ChartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(XyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Series1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(LineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GspDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GspDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'ChartControl1
-        '
-        Me.ChartControl1.DataAdapter = Me.GSPTableAdapter
-        Me.ChartControl1.DataSource = Me.GspDataSetBindingSource
-        XyDiagram1.AxisX.Label.Staggered = True
-        XyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
-        XyDiagram1.AxisX.Range.SideMarginsEnabled = True
-        XyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
-        XyDiagram1.AxisY.Range.SideMarginsEnabled = True
-        Me.ChartControl1.Diagram = XyDiagram1
-        Me.ChartControl1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ChartControl1.Legend.Visible = False
-        Me.ChartControl1.Location = New System.Drawing.Point(0, 0)
-        Me.ChartControl1.Name = "ChartControl1"
-        Series1.Name = "Series 1"
-        Series1.View = LineSeriesView1
-        Series1.ArgumentDataMember = "Region"
-        Series1.ValueDataMembersSerializable = "GSP"
-        Series1.DataFilters.ClearAndAddRange(New DevExpress.XtraCharts.DataFilter() {DataFilter1})
-        Me.ChartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {Series1}
-        Me.ChartControl1.Size = New System.Drawing.Size(518, 311)
-        Me.ChartControl1.TabIndex = 0
-        '
-        'GspDataSet
-        '
-        Me.GspDataSet.DataSetName = "gspDataSet"
-        Me.GspDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'GspDataSetBindingSource
-        '
-        Me.GspDataSetBindingSource.DataMember = "GSP"
-        Me.GspDataSetBindingSource.DataSource = Me.GspDataSet
-        '
-        'GSPTableAdapter
-        '
-        Me.GSPTableAdapter.ClearBeforeFill = True
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(518, 311)
-        Me.Controls.Add(Me.ChartControl1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
-        CType(XyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(LineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Series1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.ChartControl1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GspDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GspDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
+#End Region
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
 
-    End Sub
-    Friend WithEvents ChartControl1 As DevExpress.XtraCharts.ChartControl
-    Friend WithEvents GSPTableAdapter As ConditionallyHideSeriesLabels.gspDataSetTableAdapters.GSPTableAdapter
-    Friend WithEvents GspDataSetBindingSource As System.Windows.Forms.BindingSource
-    Friend WithEvents GspDataSet As ConditionallyHideSeriesLabels.gspDataSet
+        Private gSPTableAdapter As ConditionallyHideSeriesLabels.gspDataSetTableAdapters.GSPTableAdapter
 
-End Class
+        Private gspDataSetBindingSource As System.Windows.Forms.BindingSource
+
+        Private gspDataSet As ConditionallyHideSeriesLabels.gspDataSet
+    End Class
+End Namespace
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,19 +1,28 @@
+Imports System
+Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
+
 ' ...
+Namespace ConditionallyHideSeriesLabels
 
-Public Class Form1
+    Public Partial Class Form1
+        Inherits Form
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) _
-    Handles MyBase.Load
-        Me.GSPTableAdapter.Fill(Me.GspDataSet.GSP)
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-    End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' TODO: This line of code loads data into the 'gspDataSet.GSP' table.
+            ' You can move, or remove it, as needed.
+            gSPTableAdapter.Fill(gspDataSet.GSP)
+        End Sub
 
-    Private Sub ChartControl1_CustomDrawSeriesPoint(ByVal sender As System.Object, _
-    ByVal e As CustomDrawSeriesPointEventArgs) Handles ChartControl1.CustomDrawSeriesPoint
-        ' If the value is less than 1, hide the point's label.
-        If e.SeriesPoint(0) < 1 Then
-            e.LabelText = ""
-        End If
-    End Sub
-End Class
+        Private Sub chartControl1_CustomDrawSeriesPoint(ByVal sender As Object, ByVal e As CustomDrawSeriesPointEventArgs)
+            ' If the value is less than 1, hide the point's label.
+            If e.SeriesPoint(0) < 1 Then
+                e.LabelText = ""
+            End If
+        End Sub
+    End Class
+End Namespace
